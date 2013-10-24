954128008a3dc57c3aa16c0a71fce5c4a09a8fa3
aj heller <aj@drfloob.com>
2013-10-24 16:21:54 -0700

    implement tests for _tree.walk and walk builtins; fix bfpost
    WOO! All walk builtins are working on trivial and semi-complicated
    trees.
    
    bfpost was broken. I would have liked to implement it using the queue,
    push, and recurse methods already exposed, but I couldn't think of a
    way. Seems I needed to push children onto the stack while queueing up
    the children for evaluation themselves. I bet there's a way to do it
    with just one list, and without additional state (i.e. no additional
    counter).
    
    Also, I'm pretty confident that I understand how a breadth-first
    post-order traversal should work, but to be sure, everything's written
    and tested to work based on my understanding of the algorithm. Odd
    that I can't find much written on bfpost out there. Not sure why it's
    ignored.

--- tests/_tree.walk.spec.js
+++ tests/_tree.walk.spec.js
@@ -1,40 +1,97 @@
 /*global window, document, jasmine, describe, it, expect, beforeEach, _tree, _ */
 
 describe("_tree.walk", function () {
-    describe("with method _tree.walk.dfpre", function () {
-        it("is the default walk method", function () {
-            expect(_tree.create().defaults.walk).toEqual(_tree.walk.dfpre);
+
+    describe("on an empty tree", function () {
+        var tree;
+
+        beforeEach(function () {
+            tree = _tree.create();
         });
 
-        describe("on an empty tree", function () {
-            var tree;
-
-            beforeEach(function () {
-                tree = _tree.create();
-            });
-
+        describe("with method _tree.walk.dfpre", function () {
             it("finds only the root node", function () {
                 var ids = [];
-                tree.walk(function(n){ ids.push(n.__id); });
+                tree.walk(function(n){ ids.push(n.__id); }, _tree.walk.dfpre);
 
                 expect(ids.length).toBe(1);
                 expect(ids[0]).toBe(1);
             });
         });
 
-        describe("on a simple tree", function () {
-            var tree;
+        describe("with method _tree.walk.dfpost", function () {
+            it("finds only the root node", function () {
+                var ids = [];
+                tree.walk(function(n){ ids.push(n.__id); }, _tree.walk.dfpost);
 
-            beforeEach(function () {
-                tree = _tree.inflate([1, [2, 3, [4,5], 6]], _tree.inflate.byAdjacencyList);
+                expect(ids.length).toBe(1);
+                expect(ids[0]).toBe(1);
             });
+        });
 
-            it("finds all nodes in the correct order", function () {
-                var vals = [];
-                tree.walk(function(n){ vals.push(n.data()); });
-                expect(vals).toEqual([1,2,3,4,5,6]);
+        describe("with method _tree.walk.bfpre", function () {
+            it("finds only the root node", function () {
+                var ids = [];
+                tree.walk(function(n){ ids.push(n.__id); }, _tree.walk.bfpre);
+
+                expect(ids.length).toBe(1);
+                expect(ids[0]).toBe(1);
+            });
+        });
+
+        describe("with method _tree.walk.bfpost", function () {
+            it("finds only the root node", function () {
+                var ids = [];
+                tree.walk(function(n){ ids.push(n.__id); }, _tree.walk.bfpost);
+
+                expect(ids.length).toBe(1);
+                expect(ids[0]).toBe(1);
             });
         });
 
     });
+
+
+
+    describe("on a complicated tree", function () {
+        var tree;
+
+        beforeEach(function () {
+            tree = _tree.inflate([1, [2, 3, [4,5], 6, [7, [8, 9], 10, [11, [12]]]]], _tree.inflate.byAdjacencyList);
+        });
+
+        describe("with method _tree.walk.dfpre", function () {
+            it("finds all nodes in the correct order", function () {
+                var vals = [];
+                tree.walk(function(n){ vals.push(n.data()); }, _tree.walk.dfpre);
+                expect(vals).toEqual([1,2,3,4,5,6,7,8,9,10,11,12]);
+            });
+        });
+
+        describe("with method _tree.walk.dfpost", function () {
+            it("finds all nodes in the correct order", function () {
+                var vals = [];
+                tree.walk(function(n){ vals.push(n.data()); }, _tree.walk.dfpost);
+                expect(vals).toEqual([2,4,5,3,8,9,7,12,11,10,6,1]);
+            });
+        });
+
+        describe("with method _tree.walk.bfpre", function () {
+            it("finds all nodes in the correct order", function () {
+                var vals = [];
+                tree.walk(function(n){ vals.push(n.data()); }, _tree.walk.bfpre);
+                expect(vals).toEqual([1,2,3,6,4,5,7,10,8,9,11,12]);
+            });
+        });
+
+        describe("with method _tree.walk.bfpost", function () {
+            it("finds all nodes in the correct order", function () {
+                var vals = [];
+                tree.walk(function(n){ vals.push(n.data()); }, _tree.walk.bfpost);
+                expect(vals).toEqual([12,8,9,11,4,5,7,10,2,3,6,1]);
+            });
+        });
+
+    });
+
 });
