70e6ddf4a8c85a5ed5b5b6f895cafae77393b96c
aj heller <aj@drfloob.com>
2013-10-26 18:45:25 -0700

    grunt jshint; directory structure normalized a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,15 @@
 /* global module */
 module.exports = function(grunt) {
+
+    'use strict';
     
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
                     '    (c) 2013 A. J. Heller\n' +
-                    '    _tree.js may be freely distributed under the MIT License.\n' + 
+                    '    _tree.js may be freely distributed under the MIT License.\n' +
                     '*/\n',
                 mangle: {},
                 compress: {},
@@ -21,12 +23,27 @@ module.exports = function(grunt) {
         },
         compare_size: {
             files: ['_tree.js', 'dist/_tree.min.js']
+        },
+        jshint: {
+            src: {
+                src: ['src/**/*.js'],
+                options: { jshintrc: '.jshintrc' }
+            },
+            grunt: {
+                src: ['Gruntfile.js'],
+                options: { jshintrc: '.jshintrc' }
+            },
+            tests: {
+                src: ['test/**/*.js'],
+                options: { jshintrc: '.jshintrc' }
+            }
         }
     });
     
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-compare-size');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('default', ['uglify']);
 };
