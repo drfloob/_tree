b5de5f034c99ad573a5ea7a3af7830a7e97f3fb1
aj heller <aj@drfloob.com>
2013-10-29 02:04:43 -0700

    Tree.findByData tests for root-only, and non-unique data
    Tree.find* doesn't support walk methods. This needs to change.

--- test/_tree.findByData.spec.js
+++ test/_tree.findByData.spec.js
@@ -13,6 +13,69 @@ define(['_tree'], function (_tree) {
             it('finds nothing when searching for nothing', function () {
                 expect(tree.findByData()).toBeFalsy();
             });
+
+            it('finds nothing when searching for null', function () {
+                expect(tree.findByData(null)).toBeFalsy();
+            });
+
+            it('finds nothing when searching for the root id', function () {
+                expect(tree.findByData(0)).toBeFalsy();
+            });
+
+            it('finds nothing when searching for the tree itself', function () {
+                expect(tree.findByData(tree)).toBeFalsy();
+            });
+        });
+
+        describe('on a root-only tree', function () {
+            var tree;
+            beforeEach(function () {
+                tree = _tree.create();
+                tree = tree.root().data('funky');
+            });
+
+            it('finds nothing when searching for nothing', function () {
+                expect(tree.findByData()).toBeFalsy();
+            });
+
+            it('finds nothing when searching for null', function () {
+                expect(tree.findByData(null)).toBeFalsy();
+            });
+
+            it('finds nothing when searching for the root id', function () {
+                expect(tree.findByData(0)).toBeFalsy();
+            });
+
+            it('finds nothing when searching for the tree itself', function () {
+                expect(tree.findByData(tree)).toBeFalsy();
+            });
+            
+            it('finds the root node when searching for it', function () {
+                expect(tree.findByData('funky')).toBe(tree.root());
+            });
+
+        });
+
+
+        describe('with non-unique data', function () {
+            var tree;
+            beforeEach(function () {
+                tree = _tree.inflate([{'name': 'pops'}, [{'name': 'jr'}, {'name': 'jr'}]], _tree.inflate.byAdjacencyList);
+            });
+
+
+            it('finds the leftmost matching child by default', function () {
+                var node = tree.findByData({'name': 'jr'});
+                expect(node).toBe(tree.root().children()[0]);
+                expect(node).not.toBe(tree.root().children()[1]);
+            });
+
+            it('finds the shallowest matching child using breadth first search', function () {
+                var node = tree.findByData({'name': 'jr'});
+                throw new Error('supplying a walk fn is not supported');
+            });
+
+
         });
     });
     
