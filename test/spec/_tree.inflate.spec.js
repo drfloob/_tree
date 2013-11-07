e58cd984b1c5db80241b225d96cc72ed72191497
aj heller <aj@drfloob.com>
2013-11-07 14:40:25 -0800

    deprecate random.spec test
    The one test in it that wasn't redundant was moved to
    _tree.inflate.spec.js.

--- test/spec/_tree.inflate.spec.js
+++ test/spec/_tree.inflate.spec.js
@@ -23,32 +23,39 @@
 
     describe('_tree.inflate', function () {
         describe('.byKey', function () {
-            it('is the default', function () {
-                var tree, root;
-                tree = _tree.inflate({n: 1, children: [{n: 2}]});
+            var tree, root, data;
+            beforeEach(function () {
+                data = {n: 1, children: [{n: 2}]};
+                tree = _tree.inflate(data);
                 root = tree.root();
+            });
+            it('is the default', function () {
                 expect(root.data().n).toBe(1);
                 expect(root.children().length).toBe(1);
                 expect(root.children()[0].data().n).toBe(2);
                 expect(root.children()[0].children().length).toBe(0);
             });
             it('works with default child attribute name', function () {
-                var tree, root;
-                tree = _tree.inflate({n: 1, children: [{n: 2}]}, _tree.inflate.byKey());
-                root = tree.root();
                 expect(root.data().n).toBe(1);
                 expect(root.children().length).toBe(1);
                 expect(root.children()[0].data().n).toBe(2);
                 expect(root.children()[0].children().length).toBe(0);
             });
             it('works with different child attribute names', function () {
-                var tree, root;
-                tree = _tree.inflate({n: 1, k: [{n: 2}]}, _tree.inflate.byKey('k'));
-                root = tree.root();
                 expect(root.data().n).toBe(1);
                 expect(root.children().length).toBe(1);
                 expect(root.children()[0].data().n).toBe(2);
                 expect(root.children()[0].children().length).toBe(0);
+            });
+            it('is unaware of changes in its linked data', function () {
+                var arr = [];
+                (Object.freeze || Object)(arr);
+
+                data.b = 'test';
+                expect(tree.root().data().b).toEqual('test');
+
+                data.children = [{'name': 'newKid'}];
+                expect(tree.root().children()[0].data()).toEqual({n: 2});
             });
         });
 
