5c020fe6464f62c6233d7e7450080c1c201ddde6
aj heller <aj@drfloob.com>
2014-02-18 15:43:50 -0800

    test exposed Tree.clone method against custom classes

---
+++ test/spec/Tree.clone.spec.js
@@ -0,0 +1,32 @@
+/* global define, describe, it, expect, jasmine */
+
+(function (root, factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        define(['_tree', 'underscore'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('../../src/_tree'), require('underscore'));
+    } else {
+        factory(root._tree, root._);
+    }
+}(this, function (_tree, _) {
+    'use strict';
+
+    describe('_tree.Tree.prototype.clone', function () {
+
+        it('obeys custom classes', function () {
+            var tree, tree2, treeCls;
+            
+            treeCls = _tree.Tree.extend({one: 1});
+            tree = _tree.create({treeClass: treeCls});
+
+            tree2 = _tree.Tree.clone(tree);
+
+            expect(tree2).toEqual(jasmine.any(treeCls));
+            expect(tree2.one).toBe(1);
+        });
+
+    });
+
+}));
